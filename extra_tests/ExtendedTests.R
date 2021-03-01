2671d0f4fefea17d5cca202f736037625c70130a
Matthew Denwood <md@sund.ku.dk>
2021-03-01 08:21:57 +0100

    Add legacy extended tests and reverse checks

---
+++ extra_tests/ExtendedTests.R
@@ -0,0 +1,211 @@
+# source("C:/Users/Administrator/Desktop/runjags-tests/ExtendedTests.R")
+
+on.exit(system('rm -r /Users/matthewdenwood/Documents/Code/R/myrpackages/support/runjags-tests/testarea/*'))
+
+require('lme4')
+require('parallel')
+require('rjags')
+require('modeest')
+
+# Clear working env:
+rm(list=ls(all.names=TRUE))
+# USE PACKAGE TESTS FROM COMMAND LINE!!!!
+
+options(warn=0)
+try(unloadNamespace('runjags'))
+assign('.runjags.options', list(inits.warning=FALSE, rng.warning=FALSE, summary.warning=FALSE, nodata.warning=FALSE, blockcombine.warning=FALSE, debug=0, new.windows=FALSE), envir=.GlobalEnv)
+suppressWarnings(library('runjags'))
+
+windows <- .Platform$OS.type=='windows'
+
+if(windows){	
+  testarea <- "C:/Users/Administrator/Desktop/runjags-tests/testarea"
+  testfolder <- "C:/Users/Administrator/Desktop/runjags-tests/runjags/tests"
+  examplearea <- "C:/Users/Administrator/Desktop/runjags-tests/runjags"
+  dostandardchecks <- TRUE
+  makeForkCluster <- parallel::makeCluster
+}else{
+  testarea <- '/Users/matthewdenwood/Documents/Code/R/myrpackages/support/runjags-tests/testarea'
+  testfolder <- '/Users/matthewdenwood/Documents/GitHub/runjags/tests'
+  examplearea <- '/Users/matthewdenwood/Documents/GitHub/runjags'
+  dostandardchecks <- FALSE
+}
+
+if(!file.exists(testarea))
+	dir.create(testarea)
+
+# A function for displaying output prettily:
+fb <- function(...){
+  x <- paste(list(...),collapse='')
+  msg <- gsub('\n','',x)
+  astxs <- '*****'
+  spaces <- '   '
+  tb <- gsub('.','*',paste(astxs, msg, astxs, sep=spaces))
+  cat(paste(astxs, msg, astxs, sep=spaces), '\n', sep='')
+  invisible(TRUE)
+}
+fbb <- function(...){
+  x <- paste(list(...),collapse='')
+  msg <- gsub('\n','',x)
+  astxs <- '*****'
+  spaces <- '   '
+  tb <- gsub('.','*',paste(astxs, msg, astxs, sep=spaces))
+  cat('\n', tb,'\n',sep='')
+  cat(paste(astxs, msg, astxs, sep=spaces), '\n', sep='')
+  cat(tb,'\n',sep='')
+  invisible(TRUE)
+}
+
+
+# these tests are done on the command line:
+if(FALSE){
+  options(warn=0)  # makes warnings non-fatal  
+  
+  s <- try(devtools::install('/Users/matthewdenwood/Documents/Code/R/myrpackages/runjags'))
+  if(class(s)=='try-error')
+    stop(paste('Install failed: ', s, sep='\n'))
+  
+  
+  setwd(testarea)
+  unloadNamespace('runjags')
+  
+  print(warnings())
+  
+  library('runjags')
+  
+  # These won't run on Windows - it tries to reinstall:
+	fbb('Examples...\n')
+	s <- try({
+	devtools::run_examples(examplearea, show=FALSE, test=FALSE, run=FALSE, fresh=FALSE)
+	})
+	if(class(s)=='try-error')
+	stop(paste('Error in examples: ', s, sep='\n'))
+	graphics.off()
+
+	fbb('Examples warnings:')
+	print(warnings())
+	fbb('Done examples')
+
+	Sys.sleep(5)
+	unloadNamespace('runjags')
+
+	suppressWarnings(library('runjags'))
+
+}
+
+
+options(warn=2)  # makes warnings fatal
+
+if(dostandardchecks){
+
+	setwd(testfolder)
+	rjo <- .runjags.options
+	fbb('Check: inputs...\n')
+	s <- try({
+	  source('checkinputs.R')
+	})
+	.runjags.options <- rjo
+	if(class(s)=='try-error')
+	  stop(paste('Error in checkinputs: ', s, sep='\n'))
+
+	fbb('Check: module...\n')
+	s <- try({
+	  source('checkmodule.R')
+	})
+	if(class(s)=='try-error')
+	  stop(paste('Error in checkmodule: ', s, sep='\n'))
+
+	fbb('Check: methods...\n')
+	s <- try({
+	  source('checkmethods.R')
+	})
+	if(class(s)=='try-error')
+	  stop(paste('Error in checkmethods: ', s, sep='\n'))
+
+	fbb('Check: study...\n')
+	s <- try({
+	  source('checkstudy.R')
+	})
+	if(class(s)=='try-error')
+	  stop(paste('Error in checkmethods: ', s, sep='\n'))
+	
+	graphics.off()
+	
+}
+
+unloadNamespace('runjags')
+suppressWarnings(library('runjags'))
+
+setwd(testarea)
+s <- try({
+  fbb('Extra: utilities...\n')
+  source('../utilitiestests.R')
+})
+if(class(s)=='try-error')
+  stop(paste('Error in utilities tests: ', s, sep='\n'))
+
+s <- try({
+  fbb('Extra: check summaries...\n')
+  source('../checksummaries.R')
+})
+if(class(s)=='try-error')
+  stop(paste('Error in miscelanious tests: ', s, sep='\n'))
+
+s <- try({
+  fbb('Extra: miscelanious...\n')
+  source('../miscelanious.R')
+})
+if(class(s)=='try-error')
+  stop(paste('Error in miscelanious tests: ', s, sep='\n'))
+
+s <- try({
+  fbb('Extra: run.jags...\n')
+  source('../run.jagstests.R')
+})
+if(class(s)=='try-error')
+  stop(paste('Error in runjags tests: ', s, sep='\n'))
+
+s <- try({
+  fbb('Extra: study...\n')
+  source('../studytests.R')
+})
+if(class(s)=='try-error')
+  stop(paste('Error in run.jags.study tests: ', s, sep='\n'))
+
+s <- try({
+  fbb('Extra: glm...\n')
+  source('../glmtests.R')
+})
+if(class(s)=='try-error')
+  stop(paste('Error in glm tests: ', s, sep='\n'))
+
+s <- try({
+  fbb('Extra: 8schools...\n')
+  source('../8schools.R')
+})
+if(class(s)=='try-error')
+  stop(paste('Error in 8schools tests: ', s, sep='\n'))
+
+if(!windows){
+s <- try({
+  fbb('Paper replication...\n')
+  source('../replicationcode.R')
+})
+if(class(s)=='try-error')
+  stop(paste('Error in replication code tests: ', s, sep='\n'))
+}
+# To get rid of new settings:
+unloadNamespace('runjags')
+suppressWarnings(library('runjags'))
+
+s <- try({
+  fbb('Extra: time consuming...\n')
+  source('../timeconsuming.R')
+})
+if(class(s)=='try-error')
+  stop(paste('Error in time consuming tests: ', s, sep='\n'))
+
+unlink(testarea, recursive=TRUE)
+
+graphics.off()
+
---
+++ extra_tests/run_extra_tests.R
@@ -0,0 +1,71 @@
+## Make sure required packages are installed and updated:
+
+install.packages(c("coda","rjags","modeest","testthat","knitr","rmarkdown","spelling"), ask=FALSE)
+
+library("runjags")
+
+## Dependent functions:
+
+reverse_dependencies_with_maintainers <-
+  function(packages, which = c("Depends", "Imports", "LinkingTo", "Suggests"),
+           recursive = FALSE)
+  {
+    require("tools")
+
+    contrib.url(getOption("repos")["CRAN"], "source") # trigger chooseCRANmirror() if required
+    description <- sprintf("%s/web/packages/packages.rds",
+                           getOption("repos")["CRAN"])
+    con <- if(substring(description, 1L, 7L) == "file://")
+      file(description, "rb")
+    else
+      url(description, "rb")
+    on.exit(close(con))
+    db <- readRDS(gzcon(con))
+    rownames(db) <- NULL
+
+    rdepends <- package_dependencies(packages, db, which,
+                                     recursive = recursive,
+                                     reverse = TRUE)
+    rdepends <- sort(unique(unlist(rdepends)))
+    pos <- match(rdepends, db[, "Package"], nomatch = 0L)
+
+    db[pos, c("Package", "Version", "Maintainer")]
+  }
+
+check_reverse_dependencies <- function(package='runjags', dir=paste0('checkrevdeps_',package), which = c("Depends", "Imports", "LinkingTo", "Suggests")){
+
+  stopifnot(length(package)==1)
+
+  if(dir.exists(dir)){
+    unlink(dir, recursive=TRUE)
+  }
+
+  dir.create(dir)
+  setwd(dir)
+  dir.create('sources')
+
+  pkgs <- reverse_dependencies_with_maintainers(package, which=which, recursive=FALSE)
+
+  # Install packages with dependencies:
+  install.packages(pkgs[,'Package'], dependencies=TRUE)
+
+  # Download the packages to check:
+  download.packages(pkgs[,'Package'], destdir='_sources')
+  # And check each in a separate shell:
+  for(f in list.files('sources')){
+    system(paste0('R CMD check -o _sources ', file.path('_sources',f), ' > ', f, '.txt 2>&1 &'))
+  }
+
+  setwd('../')
+}
+
+
+## Check reverse dependencies in separate shells:
+check_reverse_dependencies()
+warning("Check reverse dependencies manually")
+## TODO: print a message to say which did not pass check cleanly
+
+
+## Run extended tests:
+source("/Documents/GitHub/runjags/extra_tests/ExtendedTests.R")
+
